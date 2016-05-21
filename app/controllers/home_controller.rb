1edfd0e5834ca8f3f3687ae89b6fddaaeee88372
a960401 <a960401sogang@gmail.com>
2016-05-21 07:39:23 +0000

    print

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
-
+  
+  def write
+    p=Userinfo.new
+    p.username=params[:username]
+    p.usernumber=params[:usernumber]
+    p.userpassword=params[:userpassword]
+    p.usertype=params[:usertype]
+    p.userdepart=params[:userdepart]
+    p.userarrive=params[:userarrive]
+    p.usertime=params[:usertime]
+    p.save
+    redirect_to "/home/complete"
+  end
   def apply
   end
 
@@ -12,6 +24,7 @@ class HomeController < ApplicationController
   end
 
   def complete
+    @userinfo=Userinfo.all
   end
 
   def reserve_list
